7275634b197962211ee28f2c862a3723038f37d3
yura91 <uvv911@mail.ru>
2023-09-01 17:52:33 +0300

    created LocationFragment.

--- app/src/main/java/com/example/multimoduleapp/GetAcquaintedFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/GetAcquaintedFragment.kt
@@ -5,18 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.multimoduleapp.databinding.FragmentGetAcquaintedBinding
 
 class GetAcquaintedFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentGetAcquaintedBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_get_acquainted, container, false)
+        binding = FragmentGetAcquaintedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.next.setOnClickListener {
+            it.findNavController().navigate(R.id.action_getAcquaintedFragment_to_locationFragment)
+        }
     }
 }
---
+++ app/src/main/java/com/example/multimoduleapp/LocationFragment.kt
@@ -0,0 +1,50 @@
+package com.example.multimoduleapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.example.multimoduleapp.databinding.FragmentLocationBinding
+
+
+class LocationFragment : Fragment() {
+    private lateinit var binding: FragmentLocationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLocationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spinnerAdapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_items,
+            android.R.layout.simple_spinner_item
+        )
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        binding.countries.adapter = spinnerAdapter;
+
+        binding.countries.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val selectedValue = parent.getItemAtPosition(position).toString()
+                // Ваш код для обработки выбранного значения
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+    }
+}
