cac8aabcf4afd97599dc32c3effff9ebf12c9fac
yura91 <uvv911@mail.ru>
2023-09-05 19:11:45 +0300

    start making Top up fragment.

---
+++ app/src/main/java/com/example/multimoduleapp/TopUpFragment.kt
@@ -0,0 +1,17 @@
+package com.example.multimoduleapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TopUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_top_up, container, false)
+    }
+}
