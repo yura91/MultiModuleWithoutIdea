fb4b422bcf92b2347f575d3c4f39ea6cb29f3ad8
yura91 <uvv911@mail.ru>
2023-09-07 12:24:20 +0300

    added alertdialog for log out.

--- app/src/main/java/com/example/multimoduleapp/SettingsFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/SettingsFragment.kt
@@ -2,6 +2,8 @@ package com.example.multimoduleapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.findNavController
 import com.example.multimoduleapp.databinding.FragmentSettingsBinding
 
@@ -16,6 +18,44 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsB
         binding?.legalInformation?.setOnClickListener {
             it.findNavController()
                 .navigate(R.id.action_settingsFragment_to_legalInformationFragment)
+        }
+
+        binding?.logOut?.setOnClickListener {
+            val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(
+                requireContext()
+            );
+
+            // set title
+            alertDialogBuilder.setTitle(
+                "Are you sure you want \n" +
+                        "to log out?"
+            );
+
+            // set dialog message
+            alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton(
+                    "Log out"
+                ) { dialog, which ->
+                    Toast.makeText(
+                        requireContext(),
+                        "You are logged out",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .setNegativeButton(
+                    "No"
+                ) { dialog, which ->
+                    Toast.makeText(
+                        requireContext(),
+                        "You are not logged out",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            val alertDialog: AlertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show()
         }
     }
 }
