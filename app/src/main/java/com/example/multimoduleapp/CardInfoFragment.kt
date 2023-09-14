b4477663c1d23eac55950392473521e1bc21f73f
yura91 <uvv911@mail.ru>
2023-09-14 11:26:29 +0300

    added necessary toasts.

--- app/src/main/java/com/example/multimoduleapp/CardInfoFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/CardInfoFragment.kt
@@ -2,6 +2,7 @@ package com.example.multimoduleapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.multimoduleapp.databinding.FragmentCardInfoBinding
@@ -37,6 +38,22 @@ class CardInfoFragment :
         backBinding?.back?.setOnClickListener {
             binding?.easyFlipView?.setFlipTypeFromBack()
             binding?.easyFlipView?.flipTheView()
+        }
+
+        backBinding?.copyCardNumber?.setOnClickListener {
+            Toast.makeText(requireContext(), "Card number is copied", Toast.LENGTH_SHORT).show()
+        }
+
+        backBinding?.copyCvv?.setOnClickListener {
+            Toast.makeText(requireContext(), "CVV is copied", Toast.LENGTH_SHORT).show()
+        }
+
+        backBinding?.copyCardHolderName?.setOnClickListener {
+            Toast.makeText(requireContext(), "CardHolder name is copied", Toast.LENGTH_SHORT).show()
+        }
+
+        backBinding?.copyExpDate?.setOnClickListener {
+            Toast.makeText(requireContext(), "Exp date is copied", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/multimoduleapp/TopUpFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/TopUpFragment.kt
@@ -1,6 +1,23 @@
 package com.example.multimoduleapp
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.multimoduleapp.databinding.FragmentTopUpBinding
 
 class TopUpFragment : BaseFragment<FragmentTopUpBinding>(FragmentTopUpBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.copyQr?.setOnClickListener {
+            Toast.makeText(requireContext(), "QR is copied", Toast.LENGTH_SHORT).show()
+        }
+        binding?.share?.setOnClickListener {
+            Toast.makeText(requireContext(), "Share id clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding?.support?.setOnClickListener {
+            Toast.makeText(requireContext(), "Support is clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
