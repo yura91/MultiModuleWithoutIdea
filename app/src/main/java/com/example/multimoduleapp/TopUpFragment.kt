4851917df624869b1816875be06b5807cd4e331a
yura91 <uvv911@mail.ru>
2023-09-18 17:54:43 +0300

    added part of view to top of TopUpFragment.

--- app/src/main/java/com/example/multimoduleapp/TopUpFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/TopUpFragment.kt
@@ -1,14 +1,56 @@
 package com.example.multimoduleapp
 
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.navigation.navGraphViewModels
 import com.example.multimoduleapp.databinding.FragmentTopUpBinding
+import com.example.multimoduleapp.viewmodels.SharedViewModel
 
 class TopUpFragment : BaseFragment<FragmentTopUpBinding>(FragmentTopUpBinding::inflate) {
+    private val layerCornerRadius = 30.0F
+    private val sharedViewModel: SharedViewModel by navGraphViewModels(R.id.nav_graph)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        sharedViewModel.gradientData.observe(viewLifecycleOwner) { gradientData ->
+            if (gradientData != null) {
+                val gradientDrawable = GradientDrawable(
+                    GradientDrawable.Orientation.TOP_BOTTOM,
+                    intArrayOf(gradientData.startColor, gradientData.endColor)
+                )
+                gradientDrawable.cornerRadius = requireContext().dpToPx(layerCornerRadius)
+                val layer1 = gradientDrawable
+                val layer2 =
+                    AppCompatResources.getDrawable(requireContext(), R.drawable.card_background_bg)
+                val layers = arrayOf(layer1, layer2)
+                val layerDrawable = LayerDrawable(layers)
+                binding?.cardInfo?.background = layerDrawable
+            }
+        }
+        binding?.cardInfo?.apply {
+            viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    val height = height
+                    val negativeMargin = (height * 0.9).toInt()
+
+                    val layoutParams = layoutParams as ViewGroup.MarginLayoutParams
+                    layoutParams.topMargin = -negativeMargin
+                    setLayoutParams(layoutParams)
+
+                    viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+        }
+
         binding?.copyQr?.setOnClickListener {
             Toast.makeText(requireContext(), "QR is copied", Toast.LENGTH_SHORT).show()
         }
@@ -19,5 +61,10 @@ class TopUpFragment : BaseFragment<FragmentTopUpBinding>(FragmentTopUpBinding::i
         binding?.support?.setOnClickListener {
             Toast.makeText(requireContext(), "Support is clicked", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun Context.dpToPx(dp: Float): Float {
+        val scale = resources.displayMetrics.density
+        return dp * scale
     }
 }
