0b600411eaeab432dafed5ce492496b72319e3fd
yura91 <uvv911@mail.ru>
2023-09-27 12:21:02 +0300

    extracted google authorization code to viewModel.

--- app/src/main/java/net/pst/cash/presentation/SignInFragment.kt
+++ app/src/main/java/net/pst/cash/presentation/SignInFragment.kt
@@ -1,10 +1,8 @@
 package net.pst.cash.presentation
 
-import android.app.Activity
 import android.os.Bundle
 import android.text.SpannableStringBuilder
 import android.text.method.LinkMovementMethod
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -13,10 +11,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.common.api.ApiException
 import dagger.hilt.android.AndroidEntryPoint
 import net.pst.cash.R
 import net.pst.cash.databinding.FragmentSignInBinding
@@ -25,10 +19,14 @@ import net.pst.cash.presentation.viewmodels.SignInViewModel
 
 @AndroidEntryPoint
 class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate) {
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signUpRequest: BeginSignInRequest
 
     private val signInViewModel: SignInViewModel by viewModels()
+    private val activityResultLauncher: ActivityResultLauncher<IntentSenderRequest> =
+        registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            signInViewModel.handleSignInResult(result)
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -37,70 +35,12 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding
         agreementSpan.append(getTermsSpan())
         binding?.textPrivacyPolicy?.text = agreementSpan
         binding?.textPrivacyPolicy?.movementMethod = LinkMovementMethod.getInstance()
-        oneTapClient = Identity.getSignInClient(requireActivity())
-        signUpRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.web_client_id))
-                    // Show all accounts on the device.
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
-            .build()
 
-        val activityResultLauncher: ActivityResultLauncher<IntentSenderRequest> =
-            registerForActivityResult(
-                ActivityResultContracts.StartIntentSenderForResult()
-            ) { result ->
-                if (result?.resultCode == Activity.RESULT_OK) {
-                    try {
-                        val credential =
-                            oneTapClient.getSignInCredentialFromIntent(result.data)
-                        val idToken = credential.googleIdToken
-                        when {
-                            idToken != null -> {
-
-                                val email = credential.id
-                                Toast.makeText(
-                                    requireContext(),
-                                    "Email: " + email,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-
-                                signInViewModel.sendTokenToBackend(idToken)
-                                // Got an ID token from Google. Use it to authenticate
-                                // with your backend.
-                                //                                         Log.d(TAG, "Got ID token.")
-                            }
-
-                            else -> {
-                                // Shouldn't happen.
-                                //                                         Log.d(TAG, "No ID token!")
-                            }
-                        }
-                    } catch (e: ApiException) {
-                        // ...
-                        e.printStackTrace()
-                    }
-
-                }
-            }
-
+        signInViewModel.signInRequest.observe(viewLifecycleOwner) { request ->
+            activityResultLauncher.launch(request)
+        }
         binding?.signWithGoogle?.setOnClickListener {
-            oneTapClient.beginSignIn(signUpRequest)
-                .addOnSuccessListener(requireActivity()) { result ->
-                    val intentSenderRequest: IntentSenderRequest =
-                        IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
-                    activityResultLauncher.launch(intentSenderRequest)
-                }
-                .addOnFailureListener(requireActivity()) { e ->
-                    // No Google Accounts found. Just continue presenting the signed-out UI.
-                    Log.d("TAG", e.localizedMessage)
-                }
-//            signInViewModel.sendTokenToBackend("eyJhbGciOiJSUzI1NiIsImtpZCI6ImI5YWM2MDFkMTMxZmQ0ZmZkNTU2ZmYwMzJhYWIxODg4ODBjZGUzYjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzNjQyOTAyMDk2MjktcDdicHMzZTJ2c2JldTNwYWN0YmYwNGRtZDZzbTVqc3EuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIzNjQyOTAyMDk2MjktMmkxb2Y2dTk5bzM2dm5hM3FydmFiajJhM2tqdmhtMjguYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDEwNDIwNzQ1Mzc3MzA4Njc2MzYiLCJlbWFpbCI6InVyaWp2bGFzaWtAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlhdCI6MTY5NTczMjI1MCwiZXhwIjoxNjk1NzM1ODUwfQ.f61uc7vxWH7uI8edmxQ-iq2_CnvAmeoYhtMufaLvKcAVp65sg1z6H71Ysr9sDMU-_Rpj5uwZr3OYTaoVKJrWb_j_d6r3q3yvNtEZTTbhEclFkToBktPmyNiAVM3IVeBfDQNIli4ykKIEad4NIuXRPSqo-6KOXSxMiDgfacJWgzmZ4DT2CE8V9u50eiabiygPIiaQIBW0uz3HWrrYNFDXMBxpJBcvX9WL_q3FD9gNbdTeyGiJyNcKDyDekxa8z-iH64jFBa4FlEoaB2q2_aDBgSpZmhwExl1B9SQYoQ48zwFV8u2UrrcxI5-1HZ-FvRx6R-yntZk6Ej6qwVM0sBZzxA\n")
-
+            signInViewModel.startSignIn()
         }
         binding?.signWithApple?.setOnClickListener {
             it.findNavController().navigate(R.id.action_signInFragment_to_getAcquaintedFragment)
--- app/src/main/java/net/pst/cash/presentation/viewmodels/SignInViewModel.kt
+++ app/src/main/java/net/pst/cash/presentation/viewmodels/SignInViewModel.kt
@@ -1,19 +1,75 @@
 package net.pst.cash.presentation.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.app.Activity
+import android.app.Application
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.common.api.ApiException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import net.pst.cash.R
 import net.pst.cash.domain.SignInInteractor
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor(private val interactor: SignInInteractor) : ViewModel() {
+class SignInViewModel @Inject constructor(
+    application: Application,
+    private val interactor: SignInInteractor
+) : AndroidViewModel(application) {
+    private val oneTapClient = Identity.getSignInClient(application)
+    private var signUpRequest: BeginSignInRequest = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(application.getString(R.string.web_client_id))
+                .setFilterByAuthorizedAccounts(false)
+                .build()
+        )
+        .build()
 
-    fun sendTokenToBackend(googleToken: String) {
+    private val _signInRequest = MutableLiveData<IntentSenderRequest>()
+    val signInRequest: LiveData<IntentSenderRequest> get() = _signInRequest
+
+    fun startSignIn() {
+        oneTapClient.beginSignIn(signUpRequest)
+            .addOnSuccessListener { result ->
+                val intentSenderRequest: IntentSenderRequest =
+                    IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+                _signInRequest.value = intentSenderRequest
+            }
+            .addOnFailureListener { e ->
+                e.localizedMessage?.let { Log.d("TAG", it) }
+            }
+    }
+
+    private fun sendTokenToBackend(googleToken: String) {
         viewModelScope.launch {
             interactor.googleSignIn(googleToken)
         }
     }
 
+    fun handleSignInResult(result: ActivityResult?) {
+        if (result?.resultCode == Activity.RESULT_OK) {
+            try {
+                val credential = oneTapClient.getSignInCredentialFromIntent(result.data)
+                val idToken = credential.googleIdToken
+                when {
+                    idToken != null -> {
+                        sendTokenToBackend(idToken)
+                    }
+
+                    else -> {
+                    }
+                }
+            } catch (e: ApiException) {
+            }
+        }
+    }
 }
