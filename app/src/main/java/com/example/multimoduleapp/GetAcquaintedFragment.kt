e267bfdf7b93e74bf305bfbcf05788a328156af8
yura91 <uvv911@mail.ru>
2023-10-03 18:22:07 +0300

    added DatePickerDialog.

--- app/src/main/java/com/example/multimoduleapp/GetAcquaintedFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/GetAcquaintedFragment.kt
@@ -1,9 +1,11 @@
 package com.example.multimoduleapp
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,6 +22,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -27,12 +30,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class GetAcquaintedFragment : Fragment() {
 
@@ -57,9 +64,43 @@ class GetAcquaintedFragment : Fragment() {
     @Preview
     @Composable
     fun GetAcquaintedLayout() {
-        var text1 by remember { mutableStateOf("") }
-        var text2 by remember { mutableStateOf("") }
-        var text3 by remember { mutableStateOf("") }
+        var firstName by remember { mutableStateOf("") }
+        var lastName by remember { mutableStateOf("") }
+        var birthDate by remember { mutableStateOf("") }
+        val dialogShowState = remember { mutableStateOf(false) }
+
+        if (dialogShowState.value) {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            // Создаем DatePickerDialog
+            val datePickerDialog = DatePickerDialog(
+                LocalContext.current, // Получаем текущий контекст
+                { _, year, month, dayOfMonth ->
+                    val date = Calendar.getInstance().apply {
+                        set(year, month, dayOfMonth)
+                    }
+                    birthDate =
+                        SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(date.time)
+                    dialogShowState.value = false
+                },
+                year,
+                month,
+                day
+            )
+
+            datePickerDialog.setOnCancelListener {
+                dialogShowState.value = false
+            }
+
+            // Отображаем DatePickerDialog
+            LaunchedEffect(Unit) {
+                datePickerDialog.show()
+            }
+        }
+
         val mainButtonColor = ButtonDefaults.buttonColors(
             containerColor = Color.Black,
             contentColor = MaterialTheme.colorScheme.surface
@@ -87,33 +128,42 @@ class GetAcquaintedFragment : Fragment() {
             )
 
             TextField(
-                value = text1,
-                onValueChange = { text1 = it },
+                value = firstName,
+                onValueChange = { firstName = it },
                 modifier = Modifier
                     .padding(start = 16.dp, end = 16.dp, top = 28.dp)
                     .fillMaxWidth(),
                 colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
-                label = { Text("Enter text") }
+                label = { Text("First name") }
             )
 
             TextField(
-                value = text2,
-                onValueChange = { text2 = it },
+                value = lastName,
+                onValueChange = { lastName = it },
                 modifier = Modifier
                     .padding(start = 16.dp, end = 16.dp, top = 28.dp)
                     .fillMaxWidth(),
                 colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
-                label = { Text("Enter text") }
+                label = { Text("Last name") }
             )
 
             TextField(
-                value = text3,
-                onValueChange = { text3 = it },
+                value = birthDate,
+                onValueChange = { birthDate = it },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.calendar),
+                        contentDescription = "Search Icon",
+                        modifier = Modifier.clickable {
+                            dialogShowState.value = true
+                        }
+                    )
+                },
                 modifier = Modifier
                     .padding(start = 16.dp, end = 16.dp, top = 28.dp)
                     .fillMaxWidth(),
                 colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
-                label = { Text("Enter text") }
+                label = { Text("Birth date") }
             )
             Spacer(Modifier.weight(1f))
             Button(
