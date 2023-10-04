35e8ca4fd98f139b933e1fec432ee44421b58ef7
yura91 <uvv911@mail.ru>
2023-10-04 15:11:59 +0300

    fixed birthdate field.

--- app/src/main/java/com/example/multimoduleapp/GetAcquaintedFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/GetAcquaintedFragment.kt
@@ -1,20 +1,25 @@
 package com.example.multimoduleapp
 
 import android.app.DatePickerDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,8 +34,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
@@ -70,9 +75,8 @@ class GetAcquaintedFragment : Fragment() {
     fun GetAcquaintedLayout() {
         var firstName by remember { mutableStateOf("") }
         var lastName by remember { mutableStateOf("") }
-        var birthDate by remember { mutableStateOf("") }
+        var birthDate by remember { mutableStateOf("Birth date") }
         val dialogShowState = remember { mutableStateOf(false) }
-        val focusRequester = remember { FocusRequester() }
 
         if (dialogShowState.value) {
             val calendar = Calendar.getInstance()
@@ -102,7 +106,10 @@ class GetAcquaintedFragment : Fragment() {
 
             // Отображаем DatePickerDialog
             LaunchedEffect(Unit) {
-                datePickerDialog.show()
+                val imm =
+                    requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                hideKeyBoard(imm, requireView(), datePickerDialog)
+//                datePickerDialog.show()
             }
         }
 
@@ -157,24 +164,23 @@ class GetAcquaintedFragment : Fragment() {
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
             )
 
-            TextField(
-                value = birthDate,
-                onValueChange = { birthDate = it },
-                leadingIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.calendar),
-                        contentDescription = "Search Icon",
-                        modifier = Modifier.clickable {
-                            dialogShowState.value = true
-                        }
-                    )
-                },
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp, top = 28.dp)
-                    .fillMaxWidth(),
-                colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
-                label = { Text("Birth date") },
-            )
+            Column {
+                Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                    .padding(start = 25.dp, end = 16.dp, top = 45.dp)
+                    .clickable {
+                        dialogShowState.value = true
+                    }) {
+                    Icon(painterResource(R.drawable.calendar), contentDescription = null)
+                    Spacer(Modifier.width(4.dp)) // Отступ между иконкой и текстом
+                    Text(birthDate)
+                }
+
+                Divider(
+                    color = Color.Black, thickness = 1.dp, modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, top = 15.dp)
+                        .fillMaxWidth()
+                )
+            }
             Spacer(Modifier.weight(1f))
             Button(
                 modifier = Modifier
@@ -190,5 +196,14 @@ class GetAcquaintedFragment : Fragment() {
                 )
             }
         }
+    }
+
+    private fun hideKeyBoard(
+        imm: InputMethodManager,
+        view: View,
+        datePickerDialog: DatePickerDialog
+    ) {
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        datePickerDialog.show()
     }
 }
