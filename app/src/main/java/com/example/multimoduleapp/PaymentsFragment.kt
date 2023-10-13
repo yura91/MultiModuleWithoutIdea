06e06dab7de9396272928edc172a0ee7536eac23
yura91 <uvv911@mail.ru>
2023-10-13 18:30:53 +0300

    fixed header.

--- app/src/main/java/com/example/multimoduleapp/PaymentsFragment.kt
+++ app/src/main/java/com/example/multimoduleapp/PaymentsFragment.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -87,11 +88,11 @@ class PaymentsFragment : Fragment() {
     ) {
         Text(
             text = text,
-            fontSize = 16.sp,
+            fontSize = 22.sp,
             fontWeight = FontWeight.Bold,
             modifier = modifier
                 .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(Color.White)
                 .padding(16.dp)
         )
     }
