e37dfc808b8e9bebae3723fb5860b16ae71944db
yura91 <uvv911@mail.ru>
2023-10-18 18:32:45 +0300

    added errorMessage livedata

--- app/src/main/java/net/pst/cash/data/repos/CardIsReadyRepo.kt
+++ app/src/main/java/net/pst/cash/data/repos/CardIsReadyRepo.kt
@@ -1,7 +1,9 @@
 package net.pst.cash.data.repos
 
+import androidx.lifecycle.LiveData
 import net.pst.cash.data.responses.CardResponseData
 
 interface CardIsReadyRepo {
+    val errorMessage: LiveData<String>
     suspend fun checkActiveCard(token: String): List<CardResponseData>?
 }
--- app/src/main/java/net/pst/cash/data/repos/CardIsReadyRepoImpl.kt
+++ app/src/main/java/net/pst/cash/data/repos/CardIsReadyRepoImpl.kt
@@ -1,5 +1,7 @@
 package net.pst.cash.data.repos
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import net.pst.cash.data.responses.ApiService
@@ -9,16 +11,26 @@ import javax.inject.Inject
 class CardIsReadyRepoImpl @Inject constructor(
     private val api: ApiService
 ) : CardIsReadyRepo {
+    private val _errorMessage: MutableLiveData<String> = MutableLiveData()
+    override val errorMessage: LiveData<String>
+        get() = _errorMessage
+
     override suspend fun checkActiveCard(token: String): List<CardResponseData>? {
         return withContext(Dispatchers.IO) {
-            val cardDataResponse = api.checkActiveCard(token)
-            if (cardDataResponse.isSuccessful) {
-                cardDataResponse.body()?.data
-            } else {
+            try {
+                val cardDataResponse = api.checkActiveCard(token)
+                if (cardDataResponse.isSuccessful) {
+                    cardDataResponse.body()?.data
+                } else {
+                    null
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _errorMessage.postValue(e.message)
                 null
             }
+
         }
 
     }
-
 }
