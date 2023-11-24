0279b73ca93c65581936d62d32b586b01bd98244
yura91 <uvv911@mail.ru>
2023-11-24 12:05:05 +0200

    set selection balance.

--- app/src/main/java/net/pst/cash/presentation/BalanceListAdapter.kt
+++ app/src/main/java/net/pst/cash/presentation/BalanceListAdapter.kt
@@ -12,10 +12,11 @@ import net.pst.cash.presentation.model.BalanceItemModel
 
 class BalanceListAdapter(private val dataSet: List<BalanceItemModel>) :
     RecyclerView.Adapter<BalanceListAdapter.ViewHolder>() {
+    private var selectedPos = RecyclerView.NO_POSITION
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val amountBalanceTextView: TextView
         val usdtTextView: TextView
-
 
         init {
             amountBalanceTextView = view.findViewById(R.id.balanceAmount)
@@ -31,12 +32,21 @@ class BalanceListAdapter(private val dataSet: List<BalanceItemModel>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (selectedPos == position) {
+            holder.amountBalanceTextView.setTextColor(holder.itemView.context.getColor(R.color.blue))
+            holder.usdtTextView.setTextColor(holder.itemView.context.getColor(R.color.blue))
+        } else {
+            holder.amountBalanceTextView.setTextColor(holder.itemView.context.getColor(R.color.black))
+            holder.usdtTextView.setTextColor(holder.itemView.context.getColor(R.color.black))
+        }
         holder.amountBalanceTextView.text = dataSet[position].balanceAmount
         holder.usdtTextView.text = dataSet[position].usdt
+        holder.itemView.setOnClickListener {
+            notifyItemChanged(selectedPos);
+            selectedPos = position;
+            notifyItemChanged(selectedPos);
+        }
     }
 
-
     override fun getItemCount() = dataSet.size
-
-
 }
