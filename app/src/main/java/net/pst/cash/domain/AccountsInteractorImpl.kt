a32ba011454da90077d3551981c42819bb7e2635
yura91 <uvv911@mail.ru>
2023-12-21 11:22:41 +0200

    request getAccounts every minute.

--- app/src/main/java/net/pst/cash/domain/AccountsInteractorImpl.kt
+++ app/src/main/java/net/pst/cash/domain/AccountsInteractorImpl.kt
@@ -1,23 +1,37 @@
 package net.pst.cash.domain
 
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.pst.cash.data.repos.AccountsRepo
 import net.pst.cash.domain.model.Account
+import java.util.Timer
+import java.util.TimerTask
 import javax.inject.Inject
+
 
 class AccountsInteractorImpl @Inject constructor(private val accountsRepo: AccountsRepo) :
     AccountsInteractor {
     private val _account = MutableLiveData<Account?>()
     override val account = _account
+    var timer = Timer()
+
     override suspend fun getAccounts(token: String) {
-        val account = accountsRepo.getAccounts(token)?.accounts?.first {
-            it.currencyId == 15
-        }
-        if (account?.addresses?.get(0)?.address != null && account.balance != null) {
-            val accountValue = Account(account.addresses[0].address!!, account.balance!!)
-            _account.value = accountValue
-        } else {
-            _account.value = null
-        }
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                GlobalScope.launch {
+                    val account = accountsRepo.getAccounts(token)?.accounts?.first {
+                        it.currencyId == 15
+                    }
+                    if (account?.addresses?.get(0)?.address != null && account.balance != null) {
+                        val accountValue =
+                            Account(account.addresses[0].address!!, account.balance!!)
+                        _account.postValue(accountValue)
+                    } else {
+                        _account.postValue(null)
+                    }
+                }
+            }
+        }, 0, 60000)
     }
 }
