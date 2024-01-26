7dd3bd3e004ab8d43649cc8dce31239501226804
yura91 <uvv911@mail.ru>
2024-01-26 16:32:24 +0200

    added some fixes with dialog fragments.

--- app/src/main/java/net/pst/cash/presentation/HelpSupportFragment.kt
+++ app/src/main/java/net/pst/cash/presentation/HelpSupportFragment.kt
@@ -3,7 +3,6 @@ package net.pst.cash.presentation
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import net.pst.cash.databinding.FragmentHelpSupportBinding
 
@@ -13,7 +12,7 @@ class HelpSupportFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding?.toolbar?.setNavigationOnClickListener {
-            it.findNavController().popBackStack()
+            findNavController().popBackStack()
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             findNavController().popBackStack()
