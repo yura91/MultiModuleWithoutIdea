eaa6614f05aa64cc990eb885346518afaa30123e
yura91 <uvv911@mail.ru>
2024-01-26 12:49:40 +0200

    added test card data.

--- app/src/main/java/net/pst/cash/presentation/CardsAdapter.kt
+++ app/src/main/java/net/pst/cash/presentation/CardsAdapter.kt
@@ -10,11 +10,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.cardview.widget.CardView
+import androidx.paging.PagingData
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.wajahatkarim3.easyflipview.EasyFlipView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.pst.cash.R
 import net.pst.cash.presentation.model.CardModel
+import net.pst.cash.presentation.model.HistoryItem
+import net.pst.cash.presentation.model.RowHistoryItems
 import net.pst.cash.presentation.model.dpToPx
 
 class CardsAdapter(
@@ -68,7 +74,8 @@ class CardsAdapter(
         val cardNumLastDigits: TextView
         val cardExpiryDate: TextView
         val easyFlipView: EasyFlipView
-
+        val simplePaymentList: RecyclerView
+        private val historyAdapter = HistoryPaymentsAdapter()
 
         init {
             cardInfoFront = itemView.findViewById(R.id.cardInfoFront)
@@ -78,6 +85,26 @@ class CardsAdapter(
             cardNumLastDigits = itemView.findViewById(R.id.cardNumLastDigits)
             cardExpiryDate = itemView.findViewById(R.id.expDate)
             easyFlipView = itemView.findViewById(R.id.easyFlipView)
+            simplePaymentList = itemView.findViewById(R.id.simplePaymentList)
+            simplePaymentList?.adapter = historyAdapter
+
+            val historyItems = mutableListOf<HistoryItem>()
+            val historyItem1 = HistoryItem("100", "Netfix", "23:55")
+            val historyItem2 = HistoryItem("200", "Netfix", "23:55")
+            val historyItem3 = HistoryItem("300", "Netfix", "00:56")
+            val historyItem4 = HistoryItem("400", "Netfix", "00:55")
+
+            historyItems.add(historyItem1)
+            historyItems.add(historyItem2)
+            historyItems.add(historyItem3)
+            historyItems.add(historyItem4)
+
+            val rowHistoryItems = mutableListOf<RowHistoryItems>()
+            val rowHistoryItem = RowHistoryItems("23.11.04", historyItems)
+            rowHistoryItems.add(rowHistoryItem)
+            CoroutineScope(Dispatchers.IO).launch {
+                historyAdapter.submitData(PagingData.from(rowHistoryItems))
+            }
         }
 
         fun setGradient() {
