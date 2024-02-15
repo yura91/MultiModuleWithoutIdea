bb9e10c788f3344efeb023127df1d7f4fb4d5421
yura91 <uvv911@mail.ru>
2024-02-15 15:19:12 +0200

    added Login by email screen.

--- app/src/main/java/net/pst/cash/presentation/SignInEmailFragment.kt
+++ app/src/main/java/net/pst/cash/presentation/SignInEmailFragment.kt
@@ -3,6 +3,7 @@ package net.pst.cash.presentation
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import net.pst.cash.R
 import net.pst.cash.databinding.FragmentSignInEmailBinding
@@ -17,17 +18,24 @@ class SignInEmailFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        signInEmailViewModel.snackBarErrorMessage.observe(viewLifecycleOwner) {
+            Snackbar.make(view, it, Snackbar.LENGTH_LONG).show();
+        }
+
+        signInEmailViewModel.emptyEmailLiveData.observe(viewLifecycleOwner) {
+            binding?.emailField?.error = getString(R.string.field_email_can_not_be_empty)
+        }
+
+        signInEmailViewModel.emptyPasswordLiveData.observe(viewLifecycleOwner) {
+            binding?.passwordField?.error = getString(R.string.field_password_can_not_be_empty)
+        }
+        signInEmailViewModel.sendSignInReqLiveData.observe(viewLifecycleOwner) {
+            signInEmailViewModel.signInEmail(it.email, it.password)
+        }
         binding?.signInButton?.setOnClickListener {
             val email = binding?.emailField?.text.toString()
             val password = binding?.passwordField?.text.toString()
-            if (email.isEmpty()) {
-                binding?.emailField?.error = getString(R.string.field_email_can_not_be_empty)
-            }
-            if (password.isEmpty()) {
-                binding?.passwordField?.error = getString(R.string.field_password_can_not_be_empty)
-            }
-
-            signInEmailViewModel.signInEmail(email, password)
+            signInEmailViewModel.validateForm(email, password)
         }
     }
 }
--- app/src/main/java/net/pst/cash/presentation/viewmodels/SignInEmailViewModel.kt
+++ app/src/main/java/net/pst/cash/presentation/viewmodels/SignInEmailViewModel.kt
@@ -2,6 +2,8 @@ package net.pst.cash.presentation.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,10 +15,36 @@ class SignInEmailViewModel @Inject constructor(
     private val application: Application,
     private val signInInteractor: SignInInteractor,
 ) : AndroidViewModel(application) {
+    val snackBarErrorMessage = signInInteractor.errorMessage
+
+    val emptyEmailLiveData: LiveData<Unit>
+        get() = _emptyEmailLiveData
+    private val _emptyEmailLiveData = MutableLiveData<Unit>()
+
+    val emptyPasswordLiveData: LiveData<Unit>
+        get() = _emptyPasswordLiveData
+    private val _emptyPasswordLiveData = MutableLiveData<Unit>()
+
+    val sendSignInReqLiveData: LiveData<EmailAndPassword>
+        get() = _sendSignInReqLiveData
+    private val _sendSignInReqLiveData = MutableLiveData<EmailAndPassword>()
 
     fun signInEmail(email: String, password: String) {
         viewModelScope.launch {
             signInInteractor.signInEmail(email, password)
         }
     }
+
+    fun validateForm(email: String, password: String) {
+        if (email.isEmpty()) {
+            _emptyEmailLiveData.value = Unit
+        } else if (password.isEmpty()) {
+            _emptyPasswordLiveData.value = Unit
+        } else {
+            _sendSignInReqLiveData.value = EmailAndPassword(email, password)
+        }
+    }
 }
+
+data class EmailAndPassword(val email: String, val password: String)
+
