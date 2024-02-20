9d0a19cf31ef744dce08133cdbbb95c444bc702e
yura91 <uvv911@mail.ru>
2024-02-20 15:16:09 +0200

    handling exception shorthistory request.

--- app/src/main/java/net/pst/cash/data/repos/HistoryListRepoImpl.kt
+++ app/src/main/java/net/pst/cash/data/repos/HistoryListRepoImpl.kt
@@ -6,6 +6,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
 import net.pst.cash.data.ApiService
 import net.pst.cash.data.paging.HistoryDataPagingSource
@@ -38,33 +39,38 @@ class HistoryListRepoImpl @Inject constructor(
         token: String,
         cardId: String
     ): Flow<Map<String, List<TransactionModel>>> {
-        val response = api.getTransactionsList(token, cardId)
-        val data = response.body()?.data
-        val transactionModels = mutableListOf<TransactionModel>()
-        val transactions = data?.take(6)?.map {
-            var dateTime = Pair("", "")
-            if (it.processedAt != null) {
-                dateTime = getDateAndTime(it.processedAt)
+        try {
+            val response = api.getTransactionsList(token, cardId)
+            val data = response.body()?.data
+            val transactionModels = mutableListOf<TransactionModel>()
+            val transactions = data?.take(6)?.map {
+                var dateTime = Pair("", "")
+                if (it.processedAt != null) {
+                    dateTime = getDateAndTime(it.processedAt)
+                }
+                var sum = ""
+                if (it.amount != null) {
+                    sum = it.amount
+                }
+                var description = ""
+                if (it.description != null) {
+                    description = it.description
+                }
+                var status: Int = 0
+                if (it.status != null) {
+                    status = it.status
+                }
+                TransactionModel(sum, description, dateTime.first, dateTime.second, status)
             }
-            var sum = ""
-            if (it.amount != null) {
-                sum = it.amount
+            if (!transactions.isNullOrEmpty()) {
+                transactionModels.addAll(transactions)
             }
-            var description = ""
-            if (it.description != null) {
-                description = it.description
-            }
-            var status: Int = 0
-            if (it.status != null) {
-                status = it.status
-            }
-            TransactionModel(sum, description, dateTime.first, dateTime.second, status)
+            val arrangedByDateModels = transactionModels.groupBy { it.datePart }
+            return flowOf(arrangedByDateModels)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return emptyFlow()
         }
-        if (!transactions.isNullOrEmpty()) {
-            transactionModels.addAll(transactions)
-        }
-        val arrangedByDateModels = transactionModels.groupBy { it.datePart }
-        return flowOf(arrangedByDateModels)
     }
 
     private fun getDateAndTime(processedAt: String): Pair<String, String> {
